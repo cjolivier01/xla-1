13bed8ffef4ac08b4c08d07f175cff7795783c2f
Chris Olivier <chris.olivier@cerebras.net>
2020-05-09 12:42:47 -0700

    .

---
+++ scripts/cerebras_generate_code.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
+XDIR="$CDIR/.."
+PTDIR="$XDIR/../pytorch"
+
+if [ -z "$1" ]; then
+  PYTORCH_BUILD_DIR=$(realpath $PTDIR/build)
+else
+  PYTORCH_BUILD_DIR=$(realpath "$1")
+fi
+
+if [ -z "$PT_INC_DIR" ]; then
+  PT_INC_DIR="$PYTORCH_BUILD_DIR/aten/src/ATen"
+fi
+
+python "$CDIR/gen.py" \
+  --gen_class_mode \
+  --output_folder="$XDIR/torch_xla/csrc" \
+  "$XDIR/torch_xla/csrc/aten_xla_type.h" \
+  "$PTDIR/torch/csrc/autograd/generated/RegistrationDeclarations.h" \
+  "$PT_INC_DIR/Functions.h" \
