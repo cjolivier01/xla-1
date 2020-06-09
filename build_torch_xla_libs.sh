a52cb4e9bcbee0ebbeb57b60de901e34b60547ca
Chris Olivier <chris.olivier@cerebras.net>
2020-06-09 14:24:47 -0700

    move grpc to other lib

--- build_torch_xla_libs.sh
+++ build_torch_xla_libs.sh
@@ -1,2 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -ex
+
+CMD="${1:-install}"
+
+cd "$(dirname "$0")"
+PWD=`printf "%q\n" "$(pwd)"`
+BASE_DIR="$PWD"
+echo $BASE_DIR
+THIRD_PARTY_DIR="$BASE_DIR/third_party"
+
+MODE="opt"
+if [[ "$XLA_DEBUG" == "1" ]]; then
+  MODE="dbg"
+fi
+
+VERBOSE=
+if [[ "$XLA_BAZEL_VERBOSE" == "1" ]]; then
+  VERBOSE="-s"
+fi
+
+OPTS=(--cxxopt="-std=c++14")
+if [[ "$CC" =~ ^clang ]]; then
+  OPTS+=(--cxxopt="-Wno-c++11-narrowing")
+fi
+
+if [[ "$XLA_CUDA" == "1" ]]; then
+  OPTS+=(--cxxopt="-DXLA_CUDA=1")
+  OPTS+=(--config=cuda)
+fi
+
+if [ "$CMD" == "clean" ]; then
+  pushd $THIRD_PARTY_DIR/tensorflow
+  /home/chriso/bin/bazel-2.0.0/bazel clean
+  popd
+else
+  cp -r -u -p $THIRD_PARTY_DIR/xla_client $THIRD_PARTY_DIR/tensorflow/tensorflow/compiler/xla/
+
+  pushd $THIRD_PARTY_DIR/tensorflow
+  /home/chriso/bin/bazel-2.0.0/bazel build $VERBOSE --define framework_shared_object=false -c "$MODE" "${OPTS[@]}" $XLA_CUDA_CFG \
+    //tensorflow/compiler/xla/xla_client:libxla_computation_client.so
+
+  popd
+  mkdir -p torch_xla/lib
+  chmod 0644 $THIRD_PARTY_DIR/tensorflow/bazel-bin/tensorflow/compiler/xla/xla_client/libxla_computation_client.so
+  cp $THIRD_PARTY_DIR/tensorflow/bazel-bin/tensorflow/compiler/xla/xla_client/libxla_computation_client.so torch_xla/lib
+fi
