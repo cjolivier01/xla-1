424f3874ee5c4958ee109134ee78754b3c1abebb
Chris Olivier <chris.olivier@cerebras.net>
2020-07-07 10:43:12 -0700

    Cleaning up TF dependencies

--- torch_xla/csrc/softmax_builder.cpp
+++ torch_xla/csrc/softmax_builder.cpp
@@ -1,5 +1,4 @@
 #include "torch_xla/csrc/softmax_builder.h"
-#include "torch_xla/csrc/lower_custom.h"
 
 #include "tensorflow/compiler/xla/client/lib/constants.h"
 #include "tensorflow/compiler/xla/xla_client/debug_macros.h"
