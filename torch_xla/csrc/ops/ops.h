6d1832032a49be50925e2bee28bd55e3868d257c
Jin Young (Daniel) Sohn <jysohn@google.com>
2020-08-24 15:09:52 -0700

    Lower `aten::round` operator (#2457)

--- torch_xla/csrc/ops/ops.h
+++ torch_xla/csrc/ops/ops.h
@@ -142,6 +142,8 @@ NodePtr Ceil(const Value& input);
 
 NodePtr Floor(const Value& input);
 
+NodePtr Round(const Value& input);
+
 NodePtr Trunc(const Value& input);
 
 NodePtr FracOp(const Value& input);
