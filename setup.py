318d96aaf5b999899bce7f39dc602b11305a3aea
Chris Olivier <chris.olivier@cerebras.net>
2020-09-06 08:07:14 -0700

    .

--- setup.py
+++ setup.py
@@ -353,7 +353,7 @@ setup(
         ],
     },
     data_files=[
-       # 'test/cpp/build/test_ptxla',
+        'test/cpp/build/test_ptxla',
         'scripts/fixup_binary.py',
     ],
     cmdclass={
