5e3e3a32d7e84b3ae918e65160a22b1df9da0487
Chris Olivier <chris.olivier@cerebras.net>
2021-08-03 06:01:48 -0700

    Allow override PYTHON_EXE

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -7,8 +7,14 @@ if [ -z "$PT_INC_DIR" ]; then
   PT_INC_DIR="$PTDIR/build/aten/src/ATen"
 fi
 
+if [ -z "$PYTHON_EXE" ]; then
+  PYTHON_EXE=python
+fi
+echo "PYTHON_EXE=$PYTHON_EXE"
+
+echo "PTDIR=$PTDIR"
 pushd $PTDIR
-python -m tools.codegen.gen_backend_stubs \
+$PYTHON_EXE -m tools.codegen.gen_backend_stubs \
   --output_dir="$XDIR/lazy_xla/csrc" \
   --source_yaml="$XDIR/xla_native_functions.yaml"\
 
