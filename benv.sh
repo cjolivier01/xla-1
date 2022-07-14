491568a926c9f508eee03b6bb339d43db1ae4f91
Chris Olivier <cjolivier01@apache.org>
2022-07-14 12:04:10 -0700

    ...

--- benv.sh
+++ benv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DEBUG=1 \
-  PATH=/home/colivier/.nvm/versions/node/v18.4.0/bin:/home/colivier/bin:/usr/local/bin:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/home/colivier/.vscode-server/bin/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/bin/remote-cli:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/home/colivier/.conda/envs/dojo/bin:/data/dojo/sw/tools/conda/conda49-py38/condabin:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin \
+  BUILD_CPP_TESTS=0 \
+  PATH=/home/${USER}/.nvm/versions/node/v18.4.0/bin:/home/${USER}/bin:/usr/local/bin:/home/${USER}/bin:/home/${USER}/scripts:/home/${USER}/.local/bin:/home/${USER}/bin:/home/${USER}/.vscode-server/bin/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/bin/remote-cli:/home/${USER}/bin:/home/${USER}/scripts:/home/${USER}/.local/bin:/home/${USER}/bin:/home/${USER}/.conda/envs/dojo/bin:/data/dojo/sw/tools/conda/conda49-py38/condabin:/home/${USER}/bin:/home/${USER}/scripts:/home/${USER}/.local/bin:/home/${USER}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin \
   $@
-
