f4fbff563e0c72eeca664711576fe9b7eafa1995
Chris Olivier <cjolivier01@apache.org>
2022-07-14 11:40:39 -0700

    ..

---
+++ benv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DEBUG=1 \
+  PATH=/home/colivier/.nvm/versions/node/v18.4.0/bin:/home/colivier/bin:/usr/local/bin:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/home/colivier/.vscode-server/bin/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/bin/remote-cli:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/home/colivier/.conda/envs/dojo/bin:/data/dojo/sw/tools/conda/conda49-py38/condabin:/home/colivier/bin:/home/colivier/scripts:/home/colivier/.local/bin:/home/colivier/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin \
+  $@
+
